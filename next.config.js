e81eeb58bedde7b265d445c84597311b97646b47
lasert0uch <seanvarvel@yahoo.com>
2021-08-29 14:37:57 -0700

    gitignore Update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://lasert0uch:F1shta1l04@cluster0.suxyl.mongodb.net/ReactReserve?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "<insert-jwt-secrets>",
     CLOUDINARY_URL: "<insert-cloudinary-url>",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
