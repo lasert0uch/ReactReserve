f42d55f07d3b06af319fed4a22060a6fad6152a8
lasert0uch <seanvarvel@yahoo.com>
2021-08-29 14:41:10 -0700

    Ignore Update

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// must restart server whenever you make changes in next.config
-module.exports = {
-  env: {
-    MONGO_SRV: "mongodb+srv://lasert0uch:F1shta1l04@cluster0.suxyl.mongodb.net/ReactReserve?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secrets>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
-  }
-};
