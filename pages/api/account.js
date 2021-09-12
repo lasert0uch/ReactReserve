61330e32d4a916c4dc83e429738f67237e1228d5
lasert0uch <seanvarvel@yahoo.com>
2021-09-12 09:46:59 -0700

    39. Change User Roles, Permissions

--- pages/api/account.js
+++ pages/api/account.js
@@ -5,10 +5,23 @@ import connectDb from '../../utils/connectDb';
 connectDb()
 
 export default async (req, res) => {
+    switch (req.method) {
+        case "GET":
+            await handleGetRequest(req, res)
+            break;
+        case "PUT":
+            await handlePutRequest(req, res)
+            break;
+        default:
+            res.status(405).send("Request not allowed: ", req.method) // Use template literals if not working
+            break;
+    }
+}
+
+async function handleGetRequest(req, res) {
     if (!("authorization" in req.headers)) { // same as (!req.headers.authorization)
         return res.status(401).send("You are not authorized for this action");
     }
-
     try {
         const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
         const user = await User.findOne({ _id: userId });
@@ -21,4 +34,13 @@ export default async (req, res) => {
         res.status(403).send("Invalid token");
     }
 
+}
+
+async function handlePutRequest(req, res) {
+    const { _id, role } = req.body;
+    await User.findOneAndUpdate(
+        { _id },
+        { role },
+    );
+    res.status(200).send('User Updated');
 }
--- pages/api/users.js
+++ pages/api/users.js
@@ -8,7 +8,17 @@ connectDb();
 export default async (req, res) => {
     try {
         const { userId } = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
-        const users = await User.find({ _id: { $ne: userId } });
+        const users = await User.find({ _id: { $ne: userId } })
+            .sort(
+                {
+                    name: "asc" //Sorting by the name of the user in the database. ascending order
+                    //for descending order write name: "desc"
+                    //Then login to your app as a root user and see the my account page
+                    //If I want to sort on the role of the user i.e., user, admin, root then
+                    //role: "asc" //for ascending order or "desc" for descending order
+                    //We can sort on email 
+                    //email: "asc" //Sort by email in ascending order
+                })
         res.status(200).json(users)
     } catch (error) {
         console.error(error)
