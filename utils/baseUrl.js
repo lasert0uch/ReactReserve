7eca7d7db633a5166a73444f67a3ab43f28bf894
lasert0uch <seanvarvel@yahoo.com>
2021-09-13 05:12:31 -0700

    Updates to baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? 'https://fathlete.herokuapp.com'
+    ? 'https://vercel.com/lasert0uch/react-reserve'
     : 'http://localhost:3000'
 
 export default baseUrl
