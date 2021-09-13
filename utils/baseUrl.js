7e07fb6d36fdab0824c3f6afdeb8b9ffe7989186
lasert0uch <seanvarvel@yahoo.com>
2021-09-13 04:57:33 -0700

    Changed BaseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,5 +1,5 @@
 const baseUrl = process.env.NODE_ENV === "production"
-    ? 'https://deployment-url.now.sh'
+    ? 'https://fathlete.herokuapp.com'
     : 'http://localhost:3000'
 
 export default baseUrl
